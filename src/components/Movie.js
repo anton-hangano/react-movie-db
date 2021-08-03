820ee059c1b69e6a6d5893ceddb2f81197e3e6ea
Anton Hangano <anton.hangano@gmail.com>
2021-08-03 23:14:17 +0200

    feat:movieInfoBar with Running Time, budget and revenue.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
 //Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
 
@@ -16,15 +17,20 @@ import NoImage from '../images/no_image.jpg'
 const Movie = () => {
     const { movieId } = useParams();
 
-    const { state: movie, loading, error} = useMovieFetch(movieId)
-    
+    const { state: movie, loading, error } = useMovieFetch(movieId)
+
     if (loading) return <Spinner />;
     if (error) return <div>Something went wrong...</div>;
 
     return (
         <>
-        <BreadCrumb movieTitle={movie.original_title} />
-        <MovieInfo movie={movie} />
+            <BreadCrumb movieTitle={movie.original_title} />
+            <MovieInfo movie={movie} />
+            <MovieInfoBar
+                time={movie.runtime}
+                budget={movie.budget}
+                revenue={movie.revenue}
+            />
         </>
     );
 };
